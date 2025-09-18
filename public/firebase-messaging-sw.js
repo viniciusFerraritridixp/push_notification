912563a397d029b51e45750966c1149785fd82fe
viniciusFerraritridixp <viniciusferrari@tridxp.com.br>
2025-09-18 08:17:52 -0300

    Adiciona suporte ao react-hot-toast para notificações em tempo real

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,13 +1,8 @@
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here. Other Firebase libraries
-// are not available in the service worker.
-// Replace 10.13.2 with latest version of the Firebase JS SDK.
+
 importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
 
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
+
 firebase.initializeApp({
 apiKey: "AIzaSyCFKfOEsVeZudq3Z-CrbKeyAgbGnadgvcs",
   authDomain: "notificacao-tridi-v3.firebaseapp.com",
@@ -18,8 +13,7 @@ apiKey: "AIzaSyCFKfOEsVeZudq3Z-CrbKeyAgbGnadgvcs",
   measurementId: "G-9470CCH7CR"
 });
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
+
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
@@ -27,7 +21,6 @@ messaging.onBackgroundMessage((payload) => {
     '[firebase-messaging-sw.js] Received background message ',
     payload
   );
-  // Customize notification here
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { useEffect } from 'react';
 import { generateToken, messaging } from './notifications/firebase';
 import { onMessage } from 'firebase/messaging';
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
 
@@ -10,24 +11,14 @@ function App() {
     generateToken();
     onMessage(messaging, (payload) => {
       console.log(payload);
-    })
+      toast(payload.notification.body);
+    });
   }, []);
 
   return (
     <div className="App">
+      <Toaster position='top-right' />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
